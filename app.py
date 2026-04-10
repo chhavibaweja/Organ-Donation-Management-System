20176731d1c5a88abb03e5fda8894f2e17f958d5
Chhavi Baweja <chhavibaweja56@gmail.com>
2026-04-10 12:53:33 +0530

    update

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session, url_for
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
@@ -327,4 +328,5 @@ def logout():
 
 # ---------- RUN ----------
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
